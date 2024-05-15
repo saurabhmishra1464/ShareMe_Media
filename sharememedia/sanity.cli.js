52f93ca226d7a19848101aea9f7657c506d39e53
saurabhmishra1464 <144934155+saurabhmishra1464@users.noreply.github.com>
2024-05-15 22:21:12 +0530

    Delete sharememedia directory

--- sharememedia/sanity.cli.js
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'c7wq1c1i',
-    dataset: 'production'
-  }
-})
--- sharememedia/schemaTypes/index.js
+++
@@ -1,6 +0,0 @@
-import User from "./User";
-import comment from "./comment";
-import pin from "./pin";
-import postedBy from "./postedBy";
-import save from "./save";
-export const schemaTypes = [User,comment,pin,postedBy,save]
